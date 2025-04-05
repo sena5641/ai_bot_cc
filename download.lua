199eb188a842881208b0d85c22c311dbfc0ed02d
Funshaped <75537163+sena5641@users.noreply.github.com>
2025-04-05 16:56:03 +0300

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,4 +1,7 @@
-shell.run("wget run https://raw.githubusercontent.com/sena5641/ai_bot_cc/refs/heads/main/load.lua")
+shell.run("wget https://raw.githubusercontent.com/sena5641/ai_bot_cc/refs/heads/main/load.lua ai/load.lua")
+shell.run("wget https://raw.githubusercontent.com/sena5641/ai_bot_cc/refs/heads/main/startup.lua startup.lua")
+
+
 -- shell.run("wget run ")
 -- shell.run("wget run ")
 -- shell.run("wget run ")
